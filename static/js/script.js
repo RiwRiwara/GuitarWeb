984a7a4e2d149a7722095a78c442857dd2889894
RiwRiwara <awirut2629@gmail.com>
2024-07-13 02:58:44 +0700

    chore: Update sound loading logic and fix sound play function

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,12 @@
-soundID = "chord_c"
+const soundIDs = ["chord_a", "chord_c", "chord_d", "chord_e", "chord_f", "chord_g"];
 
 function loadSound () {
-    createjs.Sound.registerSound("/static/sound/chords_sound/chord_c.wav", soundID);
-  }
+    soundIDs.forEach(soundID => {
+        createjs.Sound.registerSound(`/static/sound/chords_sound/${soundID}.wav`, soundID);
+    });
+}
 
-  function playSound (soundID) {
+function playSound(soundID) {
     createjs.Sound.play(soundID);
-  }
+}
 
-
-  
