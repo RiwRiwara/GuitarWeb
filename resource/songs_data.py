4b2ad14b190d4e664de4c1ba8b9395b0eae9f04c
RiwRiwara <awirut2629@gmail.com>
2024-07-14 02:56:58 +0700

    chore: Update CSS and HTML files for better styling and functionality

--- resource/songs_data.py
+++ resource/songs_data.py
@@ -8,7 +8,7 @@ songs = {
             'duration': '4:48',
             'difficult' : 'beginner',
             'genre': 'Rock',
-            'image': "/static/images/song_banner/Dontlookbackinanger.jpg"
+            'image': "/static/images/song_banner/Dontlookbackinanger.gif"
         },
         {
             'title': 'Wonderwall',
@@ -19,17 +19,21 @@ songs = {
             'image': '/static/images/song_banner/Wonderwall_cover.jpg'
         },
         {
-            'title': 'Song Title 3',
-            'artist': 'Artist Name',
-            'duration': '3:45',
-            'image': 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg'
+            'title': 'Yesterday',
+            'artist': 'The Beatles',
+            'duration': '2:05',
+            'difficult' : 'beginner',
+            'genre': 'Rock',
+            'image': '/static/images/song_banner/the-beatles-yesterday.jpg'
         },
         {
-            'title': 'Song Title 4',
-            'artist': 'Another Artist',
-            'duration': '4:20',
-            'difficult' : 'advanced',
-            'image': 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg'
+            'title': 'Mr. Brightside',
+            'artist': 'The Killers',
+            'duration': '3:42',
+            'difficult' : 'intermediate',
+            'genre': 'Rock',
+            'image': '/static/images/song_banner/Brightside.webp'
+
         },
     ],
     "set2": [
@@ -43,16 +47,20 @@ songs = {
 
         },
         {
-            'title': 'Song Title 6',
-            'artist': 'Another Artist',
-            'duration': '4:20',
-            'image': 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg'
+            'title': 'There is a light that never goes out',
+            'artist': 'The Smiths',
+            'duration': '4:03',
+            'difficult' : 'intermediate',
+            'genre': 'Rock',
+            'image': '/static/images/song_banner/thesmith1.jpg'
         },
         {
-            'title': 'Song Title 7',
-            'artist': 'Artist Name',
-            'duration': '3:45',
-            'image': 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg'
+            'title': 'Wake Me Up',
+            'artist': 'Avicii',
+            'duration': '4:09',
+            'difficult' : 'beginner',
+            'genre': 'Pop',
+            'image': '/static/images/song_banner/wakemeup.jpg'
         },
         {
             'title': 'Song Title 8',
@@ -63,22 +71,29 @@ songs = {
     ],
     "set3": [
         {
-            'title': 'Song Title 9',
-            'artist': 'Artist Name',
-            'duration': '3:45',
-            'image': 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg'
+            'title': 'Yellow',
+            'artist': 'Coldplay',
+            'duration': '4:29',
+            'difficult' : 'beginner',
+            'genre': 'Pop',
+            'image': '/static/images/song_banner/yellow.gif'
         },
         {
-            'title': 'Song Title 10',
-            'artist': 'Another Artist',
-            'duration': '4:20',
-            'image': 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg'
+            'title': 'Cruel Summer',
+            'artist': 'Taylor Swift',
+            'duration': '3:57',
+            'difficult' : 'intermediate',
+            'genre': 'Pop',
+            'image': '/static/images/song_banner/cruel.jpeg'
+
         },
         {
-            'title': 'Song Title 11',
-            'artist': 'Artist Name',
-            'duration': '3:45',
-            'image': 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg'
+            'title': 'Radioactive',
+            'artist': 'Imagine Dragons',
+            'duration': '3:07',
+            'difficult' : 'beginner',
+            'genre': 'Pop',
+            'image': '/static/images/song_banner/radioactive.gif'
         },
         {
             'title': 'Song Title 12',
@@ -86,5 +101,42 @@ songs = {
             'duration': '4:20',
             'image': 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg'
         },
+    ],
+    "set4": [
+                {
+            'title': 'Thunder',
+            'artist': 'Imagine Dragons',
+            'duration': '3:07',
+            'difficult' : 'intermediate',
+            'genre': 'Pop',
+            'image': '/static/images/song_banner/thunnder.jpg'
+
+        },
+        {
+            'title': 'Stressed Out',
+            'artist': 'Twenty One Pilots ',
+            'duration': '3:22',
+            'difficult' : 'beginner',
+            'genre': 'Alternative',
+            'image': '/static/images/song_banner/stressout.gif'
+        },
+        {
+            'title': 'Run',
+            'artist': 'LANY',
+            'duration': '3:57',
+            'difficult' : 'beginner',
+            'genre': 'Pop',
+            'image': '/static/images/song_banner/runn.jpg'
+        },
+
+        {
+            'title': 'In the end',
+            'artist': 'Linkin Park',
+            'duration': '3:36',
+            'difficult' : 'intermediate',
+            'genre': 'Rock',
+            'image': '/static/images/song_banner/intheend.webp'
+        },
+
     ]
 }
