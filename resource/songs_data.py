c1d42fdae551a3f598bfb905d5a419e379f71fa2
RiwRiwara <awirut2629@gmail.com>
2024-07-15 00:48:35 +0700

    Update song8.html, detect_page.html, home.html, and setup_canvas.html templates

--- resource/songs_data.py
+++ resource/songs_data.py
@@ -3,6 +3,7 @@
 songs = {
     "set1": [            
         {
+            'id': 'song1',
             'title': 'Dont look back in anger',
             'chords': ['C', 'G', 'A', 'E', 'F'],
             'sound' : 'https://ia800302.us.archive.org/15/items/OasisDontLookBackInAnger_201811/Oasis%20-%20Don%27t%20Look%20Back%20in%20Anger.mp3',
@@ -14,6 +15,7 @@ songs = {
             'image': "/static/images/song_banner/Dontlookbackinanger.gif"
         },
         {
+            'id': 'song2',
             'title': 'Wonderwall',
             'chords': ['F', 'A', 'D', 'E'],
             'sound' : 'https://dn720302.ca.archive.org/0/items/cd_wonderwall_oasis/disc1/01.%20Oasis%20-%20Wonderwall_sample.mp3',
@@ -25,6 +27,7 @@ songs = {
             'image': '/static/images/song_banner/Wonderwall_cover.jpg'
         },
         {
+            'id': 'song3',
             'title': 'Yesterday',
             'artist': 'The Beatles',
             'sound' : 'https://ia804701.us.archive.org/27/items/TheBeatlesYesterdayLyrics123/The%20Beatles-Yesterday%20Lyrics.mp3',
@@ -37,6 +40,7 @@ songs = {
             'image': '/static/images/song_banner/the-beatles-yesterday.jpg'
         },
         {
+            'id': 'song4',
             'title': 'Mr. Brightside',
             'artist': 'The Killers',
             'duration': '3:42',
@@ -48,6 +52,7 @@ songs = {
     ],
     "set2": [
         {
+            'id': 'song5',
             'title': 'Love Story',
             'artist': 'Taylor Swift',
             'sound' : 'https://ia903102.us.archive.org/24/items/lovestory_381/03-taylor_swift-love_story_64kb.mp3',
@@ -60,6 +65,7 @@ songs = {
 
         },
         {
+            'id': 'song6',
             'title': 'There is a light that never goes out',
             'chords': ['C', 'G', 'Am', 'F'],
             'sound' : 'https://ia600504.us.archive.org/18/items/ThereIsALightThatNeverGoesOut/TheSmiths-ThereIsALightThatNeverGoesOut.mp3',
@@ -71,6 +77,7 @@ songs = {
             'image': '/static/images/song_banner/thesmith1.jpg'
         },
         {
+            'id': 'song7',
             'title': 'Wake Me Up',
             'sound' : 'https://ezeventstorage.blob.core.windows.net/model-nsc/song1.mp3',
             'chords': ['Am', 'C', 'G', 'D'],
@@ -83,6 +90,7 @@ songs = {
         },
   
         {
+            'id': 'song8',
             'title': 'Sugar',
             'artist': 'Maroon 5',
             'sound' : 'https://ezeventstorage.blob.core.windows.net/model-nsc/song1.mp3',
@@ -97,9 +105,10 @@ songs = {
     ],
     "set3": [
         {
+            'id': 'song9',
             'title': 'Yellow',
             'artist': 'Coldplay',
-            'chords': ['C', 'G', 'Am', 'F'],
+            'chords': ['C', 'G', 'D', 'Cmaj7', 'Gsus4', 'Em7', 'Dm7'],
             'capo': 2,
             'sound' : 'https://ia600507.us.archive.org/30/items/4.11MBDownloadColdplayYellowMp3GratisStandMusic/%284.11%20MB%29%20Download%20Coldplay%20-%20Yellow%20Mp3%20Gratis%20-%20StandMusic.mp3',
             'duration': '4:29',
@@ -108,10 +117,11 @@ songs = {
             'image': '/static/images/song_banner/yellow.gif'
         },
         {
+            'id': 'song10',
             'title': 'Cruel Summer',
             'artist': 'Taylor Swift',
             'sound' : 'https://ia903108.us.archive.org/21/items/02cruelsummer/02%20Cruel%20Summer.mp3',
-            'chords': ['G', 'D', 'Am', 'C','Bm'],
+            'chords': ['G', 'D', 'Em', 'C','Bm'],
             'capo': 2,
             'duration': '3:57',
             'difficult' : 'intermediate',
@@ -120,6 +130,7 @@ songs = {
 
         },
         {
+            'id': 'song11',
             'title': 'Radioactive',
             'artist': 'Imagine Dragons',
             'sound' : 'https://ia601302.us.archive.org/28/items/Radioactive_201601/Radioactive.mp3',
@@ -131,6 +142,7 @@ songs = {
             'image': '/static/images/song_banner/radioactive.gif'
         },
         {
+            'id': 'song12',
             'title': 'Hotel California',
             'artist': 'Eagles',
             'sound' : 'https://ezeventstorage.blob.core.windows.net/model-nsc/song1.mp3',
@@ -144,7 +156,8 @@ songs = {
 
     ],
     "set4": [
-                {
+        {
+            'id': 'song13',
             'title': 'Thunder',
             'artist': 'Imagine Dragons',
             'chords': ['Am', 'C', 'G', 'D'],
@@ -157,6 +170,7 @@ songs = {
 
         },
         {
+            'id': 'song14',
             'title': 'Stressed Out',
             'artist': 'Twenty One Pilots ',
             'chords': ['Am', 'C', 'G', 'D'],
@@ -168,9 +182,10 @@ songs = {
             'image': '/static/images/song_banner/stressout.gif'
         },
         {
+            'id': 'song15',
             'title': 'Run',
             'artist': 'LANY',
-            'chords': ['Am', 'C', 'G', 'D'],
+            'chords': ['Am', 'Fmaj7/A', 'G', 'D', 'C', 'F'],
             'capo': 0,
             'sound' : 'https://ezeventstorage.blob.core.windows.net/model-nsc/song1.mp3',
             'duration': '3:57',
@@ -180,6 +195,7 @@ songs = {
         },
 
         {
+            'id': 'song16',
             'title': 'In the end',
             'artist': 'Linkin Park',
             'chords': ['Am', 'C', 'G', 'D'],
