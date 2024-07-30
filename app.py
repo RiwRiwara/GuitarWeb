4bd39a1e26f488dc7c5770849aee01a89f0a815c
RiwRiwara <awirut2629@gmail.com>
2024-07-30 20:54:41 +0700

    sdfsdfsd

--- app.py
+++ app.py
@@ -52,22 +52,22 @@ def handle_connect():
 def handle_disconnect():
     print('Client disconnected')
 
-UPLOAD_FOLDER = "./uploads/chord_am/"
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
+# UPLOAD_FOLDER = "./uploads/chord_am/"
+# if not os.path.exists(UPLOAD_FOLDER):
+#     os.makedirs(UPLOAD_FOLDER)
 
-@app.route("/upload", methods=["POST"])
-def upload_file():
-    if "file" not in request.files:
-        return jsonify({"error": "No file part"}), 400
+# @app.route("/upload", methods=["POST"])
+# def upload_file():
+#     if "file" not in request.files:
+#         return jsonify({"error": "No file part"}), 400
 
-    file = request.files["file"]
-    if file.filename == "":
-        return jsonify({"error": "No selected file"}), 400
+#     file = request.files["file"]
+#     if file.filename == "":
+#         return jsonify({"error": "No selected file"}), 400
 
-    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
-    file.save(file_path)
-    return jsonify({"filename": file.filename}), 200
+#     file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+#     file.save(file_path)
+#     return jsonify({"filename": file.filename}), 200
 
 
 if __name__ == '__main__':
