a57c90eb36d7432f3293309b41065984d8b4a49c
RiwRiwara <awirut2629@gmail.com>
2024-08-10 20:30:26 +0700

    asdasdasddsa

--- resource/song_lyrics_set/song1.py
+++ resource/song_lyrics_set/song1.py
@@ -1,18 +1,24 @@
 song1_lyrics  = [
     [   
         {
-            "time": 0,
+                "id" : "1",
+                "status" : "active",
+                "time": 0,
                 "chord": "C",
                 "lyrics": "Slip inside the",
                 
         },
         {
+                "id" : "2",
+                "status" : "active",
                 "time": 1,
                 "chord": "G",
                 "lyrics": "eye of your ",
                 
         },
         {
+                "id" : "3",
+                "status" : "active",
                 "time": 2,
                 "chord": "Am",
                 "lyrics": "mind",
@@ -21,21 +27,29 @@ song1_lyrics  = [
     ],
     [   
         {
+                "id" : "4",
+                "status" : "active",
                 "chord": "",
                 "lyrics": "Don't you "
         },
         {
+                "id" : "5",
+                "status" : "active",
                 "time": 3,
                 "chord": "E7",
                 "lyrics": "know you might"
         },
         {
+                "id" : "6",
+                "status" : "active",
                 "time": 4,
                 "chord": "F",
                 "lyrics": "find",
                 "next": 1800
         },
         {
+                "id" : "7",
+                "status" : "active",
                 "time": 5,
                 "chord": "G",
                 "lyrics": ""
@@ -43,20 +57,28 @@ song1_lyrics  = [
     ],
     [   
         {
+                "id" : "8",
+                "status" : "active",
                 "chord": "",
                 "lyrics": "A better place to"
         },
         {
+                "id" : "9",
+                "status" : "active",
                 "time": 6,
                 "chord": "C",
                 "lyrics": "play"
         },
         {
+                "id" : "9",
+                "status" : "active",
                 "time": 7,
                 "chord": "Am",
                 "lyrics": ""
         },
         {
+                "id" : "10",
+                "status" : "active",
                 "time": 8,
                 "chord": "G",
                 "lyrics": "",
@@ -65,16 +87,22 @@ song1_lyrics  = [
     ],
     [   
         {
+                "id" : "11",
+                "status" : "active",
                 "time": 9,
                 "chord": "C",
                 "lyrics": "You said that"
         },
         {
+                "id" : "12",
+                "status" : "active",
                 "time": 10,
                 "chord": "G",
                 "lyrics": "you'd never"
         },
         {
+                "id" : "13",
+                "status" : "active",
                 "time": 11,
                 "chord": "Am",
                 "lyrics": "been"
@@ -82,20 +110,28 @@ song1_lyrics  = [
     ],
     [   
         {
+                "id" : "14",
+                "status" : "active",
                 "chord": "",
                 "lyrics": "But all the "
         },
         {
+                "id" : "15",
+                "status" : "active",
                 "time": 12,
                 "chord": "E7",
                 "lyrics": "things that you've"
         },
         {
+                "id" : "16",
+                "status" : "active",
                 "time": 11,
                 "chord": "F",
                 "lyrics": "seen"
         },
         {
+                "id" : "17",
+                "status" : "active",
                 "time": 12,
                 "chord": "G",
                 "lyrics": ""
